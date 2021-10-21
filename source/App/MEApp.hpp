4616d5b5a9da8e879df8e244dd7c218ee7075e1b
Umin Lee <netuee@naver.com>
2021-10-21 21:55:47 +0900

    Revert "wip"
    This reverts commit 5a3bc97f5fc4fd6a8df7663a25800d4957484d0a.

--- source/App/MEApp.hpp
+++ source/App/MEApp.hpp
@@ -7,8 +7,6 @@
 namespace MatchEngine
 {
 
-const int MAX_FRAMES_IN_FLIGHT = 2;
-
 class MEApp
 {
 public:
@@ -16,29 +14,10 @@ public:
     static constexpr int HEIGHT = 720;
 
     void Run();
-    void DrawFrame();
 private:
     MEWindow meWindow{WIDTH,HEIGHT,"MyWindow"};
     MEDevice meDevice{meWindow};
     MEPipeline mePipeline{meDevice,meWindow,"../Shaders/Simple.vert.spv","../Shaders/Simple.frag.spv"};
-
-    void RecordCommandBuffer(int imageIndex);
-    void CraeteCommandBuffers();
-    void CreateCommandPool();
-    void CreateSyncObjects();
-
-    VkCommandBuffer BeginSingleTimeCommands();
-    void EndSingleTimeCommands(VkCommandBuffer commandBuffer);
-    
-    VkCommandPool commandPool;
-    std::vector<VkCommandBuffer> commandBuffers;
-
-    std::vector<VkFence> inFlightFences;
-    std::vector<VkFence> imagesInFlight;
-    size_t currentFrame = 0;
-
-    std::vector<VkSemaphore> imageAvailableSemaphores;
-    std::vector<VkSemaphore> renderFinishedSemaphores;
 };
 
 }
--- source/Pipeline/MEPipeline.hpp
+++ source/Pipeline/MEPipeline.hpp
@@ -9,6 +9,7 @@
 
 namespace MatchEngine
 {
+const int MAX_FRAMES_IN_FLIGHT = 2;
 
 class MEPipeline
 {
@@ -16,15 +17,19 @@ public:
     MEPipeline(MEDevice& device, MEWindow& window, const std::string& vertPath, const std::string& fragPath);
     ~MEPipeline();
 
+    void DrawFrame();
     void UpdateUniformBuffer(uint32_t currentImage);
     void RecreateSwapChain();
     void CleanupSwapChain();
 private:
     static std::vector<char> ReadFile(const std::string & path);
 
+    void CreateColorResources();
+
     void GenerateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);
     void CopyBufferToImage(VkBuffer buffer,VkImage image, uint32_t width, uint32_t height);
     void TransitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout,uint32_t mipLevels);
+    void CreateImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkSampleCountFlagBits numSample, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
     void CreateTextureImage();
 
     void CreateDescriptorSets();
@@ -32,11 +37,13 @@ private:
     void CreateDescriptorSetLayout();
 
     void CreateTextureSampler();
-
+    VkImageView CreateImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags,uint32_t mipLevels);
     void CreateTextureImageView();
-    // VkCommandBuffer BeginSingleTimeCommands();
-    // void EndSingleTimeCommands(VkCommandBuffer commandBuffer);
+    VkCommandBuffer BeginSingleTimeCommands();
+    void EndSingleTimeCommands(VkCommandBuffer commandBuffer);
     void CopyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+
+    void CreateDepthResources();
 
     void LoadModel();
 
@@ -44,12 +51,21 @@ private:
     void CreateUniformBuffers();
     void CreateIndexBuffer();
     void CreateVertexBuffer();
-    // void RecordCommandBuffer(int imageIndex);
-    // void CraeteCommandBuffers();
-    // void CreateCommandPool();
-    
+    void CreateSyncObjects();
+    void RecordCommandBuffer(int imageIndex);
+    void CraeteCommandBuffers();
+    void CreateCommandPool();
+    void CreateFrameBuffers();
     void CreateGraphicsPipeline(const std::string& vertPath, const std::string& fragPath);
     VkShaderModule CreateShaderModule(const std::vector<char>& code);
+
+    VkImage colorImage;
+    VkDeviceMemory colorImageMemory;
+    VkImageView colorImageView;
+
+    VkImage depthImage;
+    VkDeviceMemory depthImageMemory;
+    VkImageView depthImageView;
 
     uint32_t mipLevels;
     VkImageView textureImageView;
@@ -76,14 +92,18 @@ private:
     std::string vertPath;
     std::string fragPath;
 
-    //std::vector<VkCommandBuffer> commandBuffers;
-    
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::vector<VkFramebuffer> swapChainFrameBuffer;
+    std::vector<VkFence> inFlightFences;
+    std::vector<VkFence> imagesInFlight;
+    size_t currentFrame = 0;
 
-
-    //VkCommandPool commandPool;
+    VkCommandPool commandPool;
     VkPipeline graphicsPipeline;
     VkPipelineLayout pipelineLayout;
 
+    std::vector<VkSemaphore> imageAvailableSemaphores;
+    std::vector<VkSemaphore> renderFinishedSemaphores;
 
     MEDevice& device;
     MEWindow& window;
